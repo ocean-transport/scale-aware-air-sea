1598f733329be59dbef6bee48051e668456e02e9
Julius Busecke <julius@ldeo.columbia.edu>
2022-12-22 14:05:09 +0000

    massive commit before retooling to taper filter

--- notebooks/jbusecke/workflow/utils.py
+++ notebooks/jbusecke/workflow/utils.py
@@ -4,58 +4,65 @@ import numpy as np
 from tqdm.auto import tqdm
 import zarr
 
-def smooth_inputs(da:xr.DataArray, wet_mask:xr.DataArray, dims:list, filter_scale:int) -> xr.DataArray:
-    """Smoothes input using gcm-filters"""
-    input_filter = gcm_filters.Filter(
-        filter_scale=filter_scale,
-        dx_min=1,
-        filter_shape=gcm_filters.FilterShape.GAUSSIAN,
-        grid_type=gcm_filters.GridType.REGULAR_WITH_LAND,
-        grid_vars={'wet_mask': wet_mask}
-    )
-    out = input_filter.apply(da, dims=dims)
-    out.attrs['filter_scale'] = filter_scale
-    return out
+print('Use the main module for this')
 
-def smooth_inputs_dataset(ds:xr.Dataset, dims:list, filter_scale:int, timedim:str='time') -> xr.Dataset:
-    """Wrapper that filters a whole dataset, generating a wet_mask from the nanmask of the first timestep (if time is present)."""
-    ds_out = xr.Dataset()
-    for var in ds.data_vars:
-        da = ds[var]
-        if timedim in da.dims:
-            mask_da = da.isel({timedim:0})
-        else:
-            mask_da = da
+# def smooth_inputs(da:xr.DataArray, wet_mask:xr.DataArray, dims:list, filter_scale:int) -> xr.DataArray:
+#     """Smoothes input using gcm-filters"""
+#     input_filter = gcm_filters.Filter(
+#         filter_scale=filter_scale,
+#         dx_min=1,
+#         filter_shape=gcm_filters.FilterShape.GAUSSIAN,
+#         grid_type=gcm_filters.GridType.REGULAR_WITH_LAND,
+#         grid_vars={'wet_mask': wet_mask}
+#     )
+#     out = input_filter.apply(da, dims=dims)
+#     out.attrs['filter_scale'] = filter_scale
+#     return out
+
+# def smooth_inputs_dataset(ds:xr.Dataset, dims:list, filter_scale:int, timedim:str='time') -> xr.Dataset:
+#     """Wrapper that filters a whole dataset, generating a wet_mask from the nanmask of the first timestep (if time is present)."""
+#     ds_out = xr.Dataset()
+#     for var in ds.data_vars:
+#         da = ds[var]
+#         if timedim in da.dims:
+#             mask_da = da.isel({timedim:0})
+#         else:
+#             mask_da = da
             
-        wet_mask = (~np.isnan(mask_da)).astype(int)
-        ds_out[var] = smooth_inputs(da, wet_mask, dims, filter_scale)
-    return ds_out
+#         wet_mask = (~np.isnan(mask_da)).astype(int)
+#         ds_out[var] = smooth_inputs(da, wet_mask, dims, filter_scale)
+#     return ds_out
 
-def scale_separation(ds, filter_scale, mask):
-    ds_filtered = smooth_inputs_dataset(
-        ds,
-        ['yt_ocean', 'xt_ocean'],
-        filter_scale
-    )
-    diff_filtered = ds_filtered.sel(smoothing='smooth_full')-ds_filtered.sel(smoothing=['smooth_all', 'smooth_tracer', 'smooth_vel'])
-    diff_unfiltered = ds_filtered.sel(smoothing='smooth_full')-ds.sel(smoothing=['smooth_all', 'smooth_tracer', 'smooth_vel'])
+# def scale_separation(ds, filter_scale, mask):
+#     ds_filtered = smooth_inputs_dataset(
+#         ds,
+#         ['yt_ocean', 'xt_ocean'],
+#         filter_scale
+#     )
     
-    # assigne scale datasets
-    ds_full=ds_filtered.sel(smoothing='smooth_full')
+#     all_smoothing_options_except_full = [s for s in ds.smoothing.data if 'full' not in s]
     
-    ds_large_scale = ds_filtered.sel(smoothing='smooth_all')
     
-    ds_small_scale = xr.concat(
-        [
-            diff_unfiltered.sel(smoothing='smooth_all'), # the main result,
-            diff_filtered.sel(smoothing=['smooth_tracer', 'smooth_vel']), # The mechanism 'hints'
-        ],
-        dim='smoothing'
-    )
+#     diff_filtered = ds_filtered.sel(smoothing='smooth_full')-ds_filtered.sel(smoothing=all_smoothing_options_except_full)
+#     diff_unfiltered = ds_filtered.sel(smoothing='smooth_full')-ds.sel(smoothing=all_smoothing_options_except_full)
     
-    # mask the outputs
-    ds_full = ds_full.where(mask)
-    ds_large_scale = ds_large_scale.where(mask)
-    ds_small_scale = ds_small_scale.where(mask)
+#     # assigne scale datasets
+#     ds_full=ds_filtered.sel(smoothing='smooth_full')
     
-    return ds_full, ds_large_scale, ds_small_scale
+#     ds_large_scale = ds_filtered.sel(smoothing='smooth_all')
+    
+    
+#     ds_small_scale = xr.concat(
+#         [
+#             diff_unfiltered.sel(smoothing='smooth_all'), # the main result,
+#             diff_filtered.sel(smoothing=[s for s in diff_filtered.smoothing.data if 'all' not in s]), # The mechanism 'hints'
+#         ],
+#         dim='smoothing'
+#     )
+    
+#     # mask the outputs
+#     ds_full = ds_full.where(mask)
+#     ds_large_scale = ds_large_scale.where(mask)
+#     ds_small_scale = ds_small_scale.where(mask)
+    
+#     return ds_full, ds_large_scale, ds_small_scale
--- scale_aware_air_sea/utils.py
+++ scale_aware_air_sea/utils.py
@@ -1,11 +1,86 @@
 from tqdm.auto import tqdm
 import xarray as xr
 import zarr
+import gcm_filters
+import numpy as np
+
+def smooth_inputs(da:xr.DataArray, wet_mask:xr.DataArray, dims:list, filter_scale:int) -> xr.DataArray:
+    """Smoothes input using gcm-filters"""
+    input_filter = gcm_filters.Filter(
+        filter_scale=filter_scale,
+        dx_min=1,
+        filter_shape=gcm_filters.FilterShape.GAUSSIAN,
+        grid_type=gcm_filters.GridType.REGULAR_WITH_LAND,
+        grid_vars={'wet_mask': wet_mask}
+    )
+    out = input_filter.apply(da, dims=dims)
+    out.attrs['filter_scale'] = filter_scale
+    return out
+
+def smooth_inputs_dataset(ds:xr.Dataset, dims:list, filter_scale:int, timedim:str='time') -> xr.Dataset:
+    """Wrapper that filters a whole dataset, generating a wet_mask from the nanmask of the first timestep (if time is present)."""
+    ds_out = xr.Dataset()
+    
+    # create a wet mask that only allows values which are 'wet' in all variables
+    
+    
+    wet_masks = []
+    for var in ds.data_vars:
+        da = ds[var]
+        if timedim in da.dims:
+            mask_da = da.isel({timedim:0})
+        else:
+            mask_da = da
+        wet_masks.append((~np.isnan(mask_da)))
+                         
+    combined_wet_mask = xr.concat(wet_masks, dim='var').all('var').astype(int)
+    
+    for var in ds.data_vars:
+        ds_out[var] = smooth_inputs(ds[var], combined_wet_mask, dims, filter_scale)
+    
+    ds_out.attrs['filter_scale'] = filter_scale
+    return ds_out
+
+def scale_separation(ds, filter_scale, mask):
+    ds_filtered = smooth_inputs_dataset(
+        ds,
+        ['yt_ocean', 'xt_ocean'],
+        filter_scale
+    )
+    
+    all_smoothing_options_except_full = [s for s in ds.smoothing.data if 'full' not in s]
+    
+    
+    diff_filtered = ds_filtered.sel(smoothing='smooth_full')-ds_filtered.sel(smoothing=all_smoothing_options_except_full)
+    diff_unfiltered = ds_filtered.sel(smoothing='smooth_full')-ds.sel(smoothing=all_smoothing_options_except_full)
+    
+    # assigne scale datasets
+    ds_full=ds_filtered.sel(smoothing='smooth_full')
+    
+    ds_large_scale = ds.sel(smoothing='smooth_all')
+    
+    
+    ds_small_scale = xr.concat(
+        [
+            diff_unfiltered.sel(smoothing='smooth_all'), # the main result,
+            diff_filtered.sel(smoothing=[s for s in diff_filtered.smoothing.data if 'all' not in s]), # The mechanism 'hints'
+        ],
+        dim='smoothing'
+    )
+    
+    # mask the outputs
+    ds_full = ds_full.where(mask)
+    ds_large_scale = ds_large_scale.where(mask)
+    ds_small_scale = ds_small_scale.where(mask)
+    
+    return ds_full, ds_large_scale, ds_small_scale
+
 
 def to_zarr_split(ds, mapper, split_dim='time', split_interval=1):
     #
 #     if mapper.fs.exists(mapper.root):
 #         raise ValueError(f'{mapper.root} already exists. Please delete manually before writing')
+    print(f'Writing to {mapper.root} ...')
     
     n = len(ds[split_dim])
     splits = list(range(0,n,split_interval))
