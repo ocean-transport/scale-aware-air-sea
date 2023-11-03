69fabbfc1a535c47a3c348a070b4aa22ad7fb4ad
Julius Busecke <julius@ldeo.columbia.edu>
2023-11-03 19:51:44 +0000

    cleaned up old code + added plotting module

---
+++ scale_aware_air_sea/plotting.py
@@ -0,0 +1,30 @@
+import numpy as np
+import xarray as xr
+from scipy import ndimage as nd
+
+# from https://stackoverflow.com/questions/3662361/fill-in-missing-values-with-nearest-neighbour-in-python-numpy-masked-arrays
+def fill(data:np.ndarray, invalid: np.ndarray):
+    """
+    Replace the value of invalid 'data' cells (indicated by 'invalid') 
+    by the value of the nearest valid data cell
+
+    Input:
+        data:    numpy array of any dimension
+        invalid: a binary array of same shape as 'data'. True cells set where data
+                 value should be replaced.
+                 If None (default), use: invalid  = np.isnan(data)
+
+    Output: 
+        Return a filled array. 
+    """
+    #import numpy as np
+    #import scipy.ndimage as nd
+    ind = nd.distance_transform_edt(invalid, return_distances=False, return_indices=True)
+    return data[tuple(ind)]
+
+def fill_da(da: xr.DataArray) -> xr.DataArray:
+    """fills nans in dataarray"""
+    data = da.data
+    filled_data = fill(data, np.isnan(data))
+    da.data = filled_data
+    return da
